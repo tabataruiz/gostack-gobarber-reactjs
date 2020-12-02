db7e298cc56703368896b1af491a887e21c9600a
Tabata Ruiz <tabata.ruiz@rhsoftware.com.br>
2020-12-02 17:43:41 -0300

    Página de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
-import SignIn from './pages/Signin';
+import SignUp from './pages/SignUp';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyle />
   </>
 );
