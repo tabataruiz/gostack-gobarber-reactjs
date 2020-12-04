f55387e1d845c6e3121e2377cf079cc8dfe62739
Tabata Ruiz <tabata.ruiz@rhsoftware.com.br>
2020-12-04 19:43:45 -0300

    API de contexto

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 
+import AuthContext from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthContext.Provider value={{ name: 'Tabata' }}>
+      <SignIn />
+    </AuthContext.Provider>
+
     <GlobalStyle />
   </>
 );
